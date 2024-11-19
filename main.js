c0c2919620a7a25c1502bc300954013b899685fd
Uskun <90524112+Uskun25@users.noreply.github.com>
2024-11-19 15:56:37 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 //Navbar Javascript
-function toggleMenu (){
-    var menuToggle = document.querySelector ('.toggle');
-    var menu = document.querySelector ('.menu');
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    var menuToggle2 = document.querySelector('.toggle2');
     menuToggle.classList.toggle('active')
     menu.classList.toggle('active')
+    menuToggle2.classList.toggle('activena')
 }
 
 // Scroll Animation
@@ -14,21 +16,21 @@ const sr = ScrollReveal({
     reset: true
 })
 
-sr.reveal ('.home-text',{})
-sr.reveal ('.home-img',{delay:200})
-sr.reveal ('.social',{delay:200})
+sr.reveal('.home-text', {})
+sr.reveal('.home-img', { delay: 200 })
+sr.reveal('.social', { delay: 200 })
 
-sr.reveal ('.about-img',{})
-sr.reveal ('.heading',{})
-sr.reveal ('.about-text',{delay:200})
+sr.reveal('.about-img', {})
+sr.reveal('.heading', {})
+sr.reveal('.about-text', { delay: 200 })
 
-sr.reveal ('.services-content',{})
+sr.reveal('.services-content', {})
 
-sr.reveal ('.work-text',{})
-sr.reveal ('.work-img',{delay:200})
+sr.reveal('.work-text', {})
+sr.reveal('.work-img', { delay: 200 })
 
-sr.reveal ('.app h1',{})
-sr.reveal ('.app',{delay:200})
+sr.reveal('.app h1', {})
+sr.reveal('.app', { delay: 200 })
 
-sr.reveal ('form input',{})
-sr.reveal ('form textarea',{})
+sr.reveal('form input', {})
+sr.reveal('form textarea', {})
