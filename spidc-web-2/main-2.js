3bbf8dcb92007c04dd3819742e2944538d21f763
Uskun <90524112+Uskun25@users.noreply.github.com>
2024-11-20 09:54:19 +0800

    Add files via upload

---
+++ spidc-web-2/main-2.js
@@ -0,0 +1,23 @@
+// Scroll Animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home', {})
+sr.reveal('.home-text', { delay: 500 })
+
+sr.reveal('.about-img', {})
+sr.reveal('.heading', {})
+sr.reveal('.about-text', { delay: 500 })
+
+sr.reveal('.sales', {})
+
+sr.reveal('.properties-container', {delay:200})
+
+sr.reveal('.newsletter h2', {})
+sr.reveal('.newsletter ', { delay: 200 })
+
+sr.reveal('copyright', {})
